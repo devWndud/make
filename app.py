10f6f037c1204820fa805137b47ea9ddb1b419b4
devWndud <devwndud@gmail.com>
2025-05-30 20:47:24 +0900

    test8

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +6,16 @@ app = Flask(__name__)
 @app.route('/test', methods=['POST', 'GET'])
 def Test():
     return jsonify({'test': True}), 200
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    note = data.get('note', '')
+    return jsonify({'summary': note}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  
