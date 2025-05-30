60e0f061737d55cbe5b3bd418fa8696e0bd5de53
devWndud <devwndud@gmail.com>
2025-05-30 18:25:52 +0900

    test6

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
+
 from flask import Flask, jsonify
-from flask_cors import CORS
-import traceback
-import os
 
 app = Flask(__name__)
-CORS(app)
 
 
 @app.route('/test', methods=['POST', 'GET'])
 def Test():
-    return 1
+    return jsonify({'test': True}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)  
+    app.run(host='0.0.0.0')  
